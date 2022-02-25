fba18e6ededbb7f9be419dc1bf1744e52073f11d
Carlos G <cgonz490@stu.southtexascollege.edu>
2022-02-25 10:29:53 -0600

    local changes

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-<div class="min-h-screen bg-slate-400">
+<div class="min-h-screen bg-slate-300">
     <Nav />
     <div class="container mx-auto grid justify-items-center">
      <router-view />
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,14 +1,14 @@
 <template>
-<div class="bg-gray-800 px-6">
-    <div class="conitainer mx-auto flex justify-between items-center text-white">
-        <router-link to="/"><h1 class="tracking-tighter py-6 text-4xl font-light text-green-600 hover:cursor-pointer">
-            Basic<span class="font-semibold">Router</span> 
+<div class="bg-cyan-700 px-6">
+    <div class="conitainer mx-auto flex justify-between items-center">
+        <router-link to="/"><h1 class="tracking-tighter py-6 text-4xl font-light text-black hover:cursor-pointer">
+            Vue<span class="font-semibold">FakeAuth</span> 
         </h1></router-link>
         <nav>
-            <ul class="flex space-x-4 text-gray-400">
-                <router-link to="/"><li class="py-8 px-4 hover:cursor-pointer hover:bg-gray-700 hover:text-gray-200">Home</li></router-link>
-                <router-link to="/about"><li class="py-8 px-4 hover:cursor-pointer hover:bg-gray-700 hover:text-gray-200">About</li></router-link>
-                <router-link to="/contact"><li class="py-8 px-4 hover:cursor-pointer hover:bg-gray-700 hover:text-gray-200">Contact</li></router-link>
+            <ul class="flex space-x-4 text-gray-800">
+                <router-link to="/"><li class="py-8 px-4 hover:cursor-pointer hover:bg-cyan-800 hover:text-gray-200">Home</li></router-link>
+                <router-link to="/about"><li class="py-8 px-4 hover:cursor-pointer hover:bg-cyan-800 hover:text-gray-200">About</li></router-link>
+                <router-link to="/contact"><li class="py-8 px-4 hover:cursor-pointer hover:bg-cyan-800 hover:text-gray-200">Contact</li></router-link>
             </ul>
         </nav>
     </div>
