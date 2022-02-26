9c359156c313691f254019045a4a70e4e4714f45
Carlos G <cgonz490@stu.southtexascollege.edu>
2022-02-26 12:06:17 -0600

    finished

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -2,6 +2,10 @@
     import { ref } from "vue";
     import { useRouter } from "vue-router";
     import userAuth from "../composables/userAuth";
+    import loginError from "../composables/loginError";
+    
+
+
     const {isAuthenticated, login} = userAuth();
 
     const username = ref("");
@@ -18,6 +22,13 @@
             start();
     }
 };
+
+const { error, setError } = loginError()
+
+import { useTimeout, promiseTimeout } from "@vueuse/core";
+
+const { ready, start } = useTimeout(3000, { controls: true });
+
 </script>
 
 <template>
@@ -26,10 +37,17 @@
   <div class="shadow-2xl border-1 rounded-md flex items-center justify-center bg-slate-300 overflow-hidden">
   <img src="../assets/loginBG.png" alt="bg-image" class="h-48">
   <form @submit.prevent="logginIn" class="flex flex-col space-y-4 p-3">
-      <input type="text" class="border-1 p-1 rounded-md" placeholder="Username" v-model="username"/>
-      <input type="password" class="border-1 p-1 rounded-md" placeholder="Password" v-model="password"/>
+      <input type="text" placeholder="Username" v-model="username"
+      :class="!ready && error ? 'border-2 border-red-500 p-1 rounded-md' : 'p-1 rounded-md'"
+      />
+      <input type="password" placeholder="Password" v-model="password"
+      :class="!ready && error ? 'border-2 border-red-500 p-1 rounded-md' : 'p-1 rounded-md'"
+      />
       <button type="submit" @submit.prevent="logginIn" class="bg-cyan-700 text-black py-1 rounded-md">Login</button>
   </form>
+  </div>
+  <div v-if="!ready && error" class="absolute w-1/3 px-4 py-3 text-center text-red-800 bg-red-300 rounded-lg bottom-2 right-2 shadow-inner">
+      {{ error }}
   </div>
 </div>
 </template>
